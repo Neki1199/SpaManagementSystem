b6f1175a4b95aeb8fc7ffce1c79292a1ce3f018c
Neki1199 <neki.cas.al@gmail.com>
2024-09-05 10:49:27 +0100

    First time initialized implemented

--- src/main/java/com/sms/Controllers/LoginController.java
+++ src/main/java/com/sms/Controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.sms.Controllers;
 import com.sms.DAO.EmpDAOImplement;
 import com.sms.DAO.EmployeeDAO;
 import com.sms.Model;
+import com.sms.Models.Employee;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -11,6 +12,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
@@ -29,46 +32,46 @@ public class LoginController implements Initializable {
             onLogin();
         });
         // Creates an instance of ViewFactory to show view of Staff (Staff Menu)
-//        loginErrorLabel.setVisible(false);
-//
-//        List<Employee> employeeList;
-//        try {
-//            employeeList = employeeDAO.getAll();
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        loginButton.setOnAction(event -> {
-//            // get username and password
-//            String username = userInput.getText();
-//            String password = passwordInput.getText();
-//            Employee employee = null;
-//
-//            for (Employee employeeTemp : employeeList) {
-//                if (employeeTemp.getUsername().equals(username) && employeeTemp.getPassword().equals(password)) {
-//                    employee = employeeTemp;
-//                } else if(employeeTemp.getUsername().equals(username) && !employeeTemp.getPassword().equals(password)) {
-//                    employee = employeeTemp;
-//                }
-//            }
-//
-//            if(employee != null && employee.getUsername().equals(username) && employee.getPassword().equals(password)){
-//                if(employee.getAdmin() == 1) {
-//                    Model.getInstance().getViewFactory().showAdminAppointments(); // Enter admin
-//                }else{
-//                    Model.getInstance().getViewFactory().showStaffAppointments(); // Enter staff
-//                }
-//            } else{
-//                if(!username.isEmpty() && !password.isEmpty()){
-//                    loginErrorLabel.setText("Error: incorrect details");
-//                    loginErrorLabel.setVisible(true);
-//                } else{
-//                    loginErrorLabel.setText("Error: enter details");
-//                    loginErrorLabel.setVisible(true);
-//                }
-//            }
-//        onLogin();
-//        });
+        loginErrorLabel.setVisible(false);
+
+        List<Employee> employeeList;
+        try {
+            employeeList = employeeDAO.getAll();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        loginButton.setOnAction(event -> {
+            // get username and password
+            String username = userInput.getText();
+            String password = passwordInput.getText();
+            Employee employee = null;
+
+            for (Employee employeeTemp : employeeList) {
+                if (employeeTemp.getUsername().equals(username) && employeeTemp.getPassword().equals(password)) {
+                    employee = employeeTemp;
+                } else if(employeeTemp.getUsername().equals(username) && !employeeTemp.getPassword().equals(password)) {
+                    employee = employeeTemp;
+                }
+            }
+
+            if(employee != null && employee.getUsername().equals(username) && employee.getPassword().equals(password)){
+                if(employee.getAdmin() == 1) {
+                    Model.getInstance().getViewFactory().showAdmin(); // Enter admin
+                }else{
+                    Model.getInstance().getViewFactory().showStaff(); // Enter staff
+                }
+            } else{
+                if(!username.isEmpty() && !password.isEmpty()){
+                    loginErrorLabel.setText("Error: incorrect details");
+                    loginErrorLabel.setVisible(true);
+                } else{
+                    loginErrorLabel.setText("Error: enter details");
+                    loginErrorLabel.setVisible(true);
+                }
+            }
+        onLogin();
+        });
     }
     private void onLogin(){
         // to retrieve the stage of that object (if not, is not possible to get the stage)
